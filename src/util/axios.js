66d89ddce14b88f9f16e5fa18d4feadc6af353d8
human1123 <selliott522@gmail.com>
2021-02-04 19:28:51 -0700

    Updated the main axiosInstance from local to dev

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: 'https://medication-track.herokuapp.com/api/',
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
