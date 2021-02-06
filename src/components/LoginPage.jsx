4390c4e75dd810a20085c5944c73eb46e4aacab3
human1123 <selliott522@gmail.com>
2021-02-05 20:44:50 -0700

    Added form validation to the register and login pages.

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import axiosInstance from "../util/axios";
 
-import { Button, Card, Form } from "react-bootstrap";
+import { Button, Card, Form, Col } from "react-bootstrap";
 
 function LoginPage(props) {
   const checkForToken = props.checkForToken;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [errMsg, setErrMsg] = useState({
+    email: null,
+    password: null,
+  });
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -19,7 +24,23 @@ function LoginPage(props) {
         sessionStorage.setItem("Token", token);
         checkForToken();
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error.response.data);
+        handleErrMsg(error.response.data);
+      });
+  }
+
+  function handleErrMsg(responseData) {
+    if (responseData.email) {
+      setErrMsg((prev) => {
+        return { ...prev, email: responseData.email[0] };
+      });
+    }
+    if (responseData.password) {
+      setErrMsg((prev) => {
+        return { ...prev, password: responseData.password[0] };
+      });
+    }
   }
 
   function goToRegisterPage() {
@@ -28,47 +49,58 @@ function LoginPage(props) {
 
   return (
     <>
-      <Card className="login-card">
-        <Card.Header>
-          <Card.Title as={"h1"}>Welcome Back</Card.Title>
-          <Card.Subtitle>Please sign in</Card.Subtitle>
-        </Card.Header>
-        <Card.Body>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                // required
-                type="email"
-                placeholder="Email"
-                autoComplete="username"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter a valid email.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                // required
-                // minLength={4}
-                type="password"
-                placeholder="Password"
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
+      <Col
+        xs={10}
+        sm={8}
+        md={6}
+        lg={5}
+        xl={4}
+        style={{ margin: "auto", marginTop: "2rem", marginBottom: "2rem" }}
+      >
+        <Card className="login-card">
+          <Card.Header>
+            <Card.Title as={"h1"}>Welcome Back</Card.Title>
+            <Card.Subtitle>Please sign in</Card.Subtitle>
+          </Card.Header>
+          <Card.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  className={errMsg.email ? "is-invalid" : "is-valid"}
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="username"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errMsg.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  className={errMsg.password ? "is-invalid" : "is-valid"}
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errMsg.password}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
 
-        <Button onClick={goToRegisterPage}>Register</Button>
-      </Card>
+          <Button onClick={goToRegisterPage}>Register</Button>
+        </Card>
+      </Col>
     </>
   );
 }
