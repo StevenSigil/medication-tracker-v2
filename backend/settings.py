3c61b52adbbfd4ebc8e7406ad4c50a5fcf653642
human1123 <selliott522@gmail.com>
2022-12-12 14:59:48 -0500

    Changed API base URL for newest platform.

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ssigil.pythonanywhere.com']  # ['https://medication-track.herokuapp.com/']  # ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ssigil.pythonanywhere.com']  # ['localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
--- src/util/axios.js
+++ src/util/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-  // baseURL: null, // 'https://medication-track.herokuapp.com/api/',
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://ssigil.pythonanywhere.com/api/", // 'https://medication-track.herokuapp.com/api/',
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
